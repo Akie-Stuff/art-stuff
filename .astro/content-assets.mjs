a236ffaf0b24f44aea7f90d1568d5997884d3ecd
akiekano12 <akiekano12@gmail.com>
2026-06-11 11:39:01 +0700

    add change post

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_2ddohF from "../../assets/blog-placeholder-5.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
-import __ASTRO_IMAGE_IMPORT_UayhY from "../../assets/blog-placeholder-3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
-export default new Map([["../../assets/blog-placeholder-5.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_2ddohF], ["../../assets/blog-placeholder-3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_UayhY]]);
+import __ASTRO_IMAGE_IMPORT_73MW2 from "../../assets/blog-placeholder-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
+export default new Map([["../../assets/blog-placeholder-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_73MW2]]);
 		
